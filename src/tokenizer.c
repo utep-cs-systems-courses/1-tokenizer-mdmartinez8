cd9aa504106d02642348b47b366bdce34fde22e5
student <mdmartinez8@miners.utep.edu>
2023-02-19 15:47:11 -0700

    Progress as of 2/19

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,71 +3,95 @@
 #include "tokenizer.h"
 
 
-int space_char(char c)
+int space_char(char c)     //Returns 1 if whitespace
 {
-  if (c == ' ' || c == '\t')
+  if (c == ' ' || c == '\t' || c == '\n')
     return 1;
   return 0;
 }
 
-int non_space_char(char c)
+int non_space_char(char c)     //Returns 1 if not a whitespace
 {
   if (space_char(c) == 0)
     return 1;
   return 0;
 }
 
-char *word_start(char *str)
+char *word_start(char *str)    //Returns pointer to first char in word, or NULL if no word exists
 {
   while(*str != '\0')
   {
     if (non_space_char(*str) == 1)
       return str;
+    str++;
   }
 
-  return NULL;
+  return 0;
 }
 
-char *word_terminator(char *word)
+char *word_terminator(char *word)     //Returns pointer to last char in word
 {
-  char *temp = word_start(word);
-  if (temp == NULL)
+  while(*word != '\0' && non_space_char(*word) == 1)
   {
-    return NULL;
-  }else
-  {
-    while(*temp != '\0')
-      temp++;
+    word++;
   }
-  return temp;
+  word--;
+  return word;
 }
 
 
 int count_words(char *str)
 {
+  int count = 0;
 
-
+  while (*str != '\0')
+  {
+    if ((str = word_start(str)))
+      count++;
+    str = word_terminator(str);
+    str++;
+  }
+  return count;
 }
 
 
 char *copy_str(char *intr, short len)
 {
-
-
+  char *strCopy = (char *)malloc((len)*sizeof(char));
+  while (*intr != '\0' && non_space_char(*intr) == 1)
+  {
+    *strCopy++ = *intr++;
+  }
+  return strCopy;
 }
 
 
 char **tokenize(char *str)
 {
-
-
+  int count = count_words(str);
+  char **tokens = (char **)malloc((count+1) * sizeof(char *));
+  int i;
+  for (i = 0; i < count; i++)
+  {
+    str = word_start(str);
+    int wordLen = word_terminator(str) - str;
+    tokens[i] = copy_str( str, (wordLen+1));
+    str = word_terminator(str);
+    str++;
+  }
+  i++;
+  tokens[i] = 0;
+  return tokens;
 }
 
 
 void print_tokens(char **tokens)
 {
-
-
+  while (*tokens != 0)
+  {
+    printf("%s\n", *tokens);
+    *tokens++;
+  }
 }
 
 
--- src/uimain.c
+++ src/uimain.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "tokenizer.h"
+#include "tokenizer.h"
 //#include "history.h"
 #define maxLength 1000
 
@@ -8,6 +8,7 @@ int main()
 {
   int isRunning = 1;
   char *input = (char *)malloc(maxLength*sizeof(char));
+  
   while(isRunning)
   {
     fputs("Enter string to save.\tEnter !1 to view history.\n", stdout);
@@ -18,9 +19,13 @@ int main()
       isRunning = 0;
       break;
     }
-    printf("%d", space_char(*input));
+    char **tokens = tokenize(input);
+    print_tokens(tokens);
+    //Test  printf("%s\n", word_start(input));
+    //Test  printf("%s\n", word_terminator(input));
+    //Test  printf("%d\n", count_words(input));
     
-    //printf("%s\n\n", input);
+    printf("%s\n\n", input);
 
 
   }
