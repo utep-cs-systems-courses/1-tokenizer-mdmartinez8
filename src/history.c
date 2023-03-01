bd84d1b3089432affa372ba745e99c688daedecd
student <mdmartinez8@miners.utep.edu>
2023-03-01 08:48:01 -0700

    Progress to history 3/1

--- src/history.c
+++ src/history.c
@@ -1,1 +1,47 @@
 #include "history.h"
+
+
+
+List* init_history()
+{
+  List* history;
+  history->root->id = 0;
+  return history;
+}
+
+void add_history(List *list, char *str)
+{
+  Item *temp = list->root;
+  if (temp->id == 0)
+  {
+    temp->str = str;
+    temp->id += 1;
+    return;
+  }
+
+  while (temp->next)
+  {
+    temp = temp->next;
+  }
+  temp->next->str = str;
+  temp->next->id = temp->id + 1;
+  return;
+}
+
+char *get_history(List *list, int id)
+{
+
+
+}
+
+void print_history(List *list)
+{
+
+
+}
+
+void free_history(List *list)
+{
+
+
+}
--- src/uimain.c
+++ src/uimain.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
-//#include "history.h"
+#include "history.h"
 #define maxLength 1000
 
 int main()
 {
   int isRunning = 1;
   char *input = (char *)malloc(maxLength*sizeof(char));
-  
+  List* history = init_history();
+  //fputs("%d", history->root->id);
   while(isRunning)
   {
     fputs("Enter string to save.\tEnter !1 to view history.\n", stdout);
@@ -19,6 +20,7 @@ int main()
       isRunning = 0;
       break;
     }
+    
     char **tokens = tokenize(input);
     print_tokens(tokens);
     //Test  printf("%s\n", word_start(input));
